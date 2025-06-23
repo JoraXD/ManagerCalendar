1f382b7854f01c01bd844fdd4b86dc4bda6c438b
Georgii Beliaev <123897400+JoraXD@users.noreply.github.com>
2025-06-23 17:39:52 +0300

    згыр

--- backend/main_firestore.py
+++ backend/main_firestore.py
@@ -8,7 +8,7 @@ import uvicorn
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred_path = os.getenv("FIREBASE_CREDENTIALS")
+cred_path = os.getenv(".venv/google-services.json")
 if cred_path:
     cred = credentials.Certificate(cred_path)
     firebase_admin.initialize_app(cred)
